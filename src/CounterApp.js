ab8234b182149c38f6b79edbbac41f17fc8c56ab
Alonso <alonso.amoreno@live.com>
2020-07-11 11:17:40 -0500

    primer Commit

---
+++ src/CounterApp.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+
+
+const CounterApp = ({ value }) => {
+
+    const [contador, setContador] = useState(value)
+
+    // handleAdd
+    const handleAdd = () => setContador(contador + 1)
+
+    const handleSubstract = () => setContador(contador - 1)
+
+    const handleReset = () => setContador(value)
+
+    return (
+        <div>
+            <h1>Counter App</h1>
+            <h2>{contador}</h2>
+            <button onClick={handleAdd}>+1</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleSubstract}>-1</button>
+        </div>
+    )
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
+
+export default CounterApp
+
+// rafcp(snippet para generar el functional component)
---
+++ src/PrimeraApp.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import PropTypes from 'prop-types'
+// import React, { Fragment } from 'react';
+// Functional Components
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
+
+    // Propiedades, props
+
+    return (
+        <>
+            <h1>{saludo}</h1>
+            {/* <pre>{JSON.stringify(saludo, null, 3)}</pre> */}
+            <p>{subtitulo}</p>
+        </>
+    )
+
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
+}
+
+
+export default PrimeraApp
